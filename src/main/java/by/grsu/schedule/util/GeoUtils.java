0ce2f6e09f8bc22d35914152b8a9c91cd6ebc417
Pavel Shareiko <81857881+pavel-shareiko@users.noreply.github.com>
2024-04-20 19:56:59 +0300

    fix "NaN" when distance between two coordinates is 0 (#8)
    Co-authored-by: Pavel Shareika <pavel.shareika@azati.com>

--- src/main/java/by/grsu/schedule/util/GeoUtils.java
+++ src/main/java/by/grsu/schedule/util/GeoUtils.java
@@ -7,11 +7,15 @@ import org.springframework.data.geo.Metrics;
 
 @UtilityClass
 public class GeoUtils {
-    public static final int EARTH_RADIUS_IN_KM = 6371;
     public static final Distance ZERO_DISTANCE = new Distance(0.0);
+    public static final int EARTH_RADIUS_IN_KM = 6371;
     public static final double AVERAGE_WALKING_SPEED_IN_KMH = 5.0;
 
     public static Distance calculateDistance(Coordinate first, Coordinate second) {
+        if (first.equals(second)) {
+            return ZERO_DISTANCE;
+        }
+
         double lat1 = Math.toRadians(first.getLatitude());
         double lat2 = Math.toRadians(second.getLatitude());
         double lon1 = Math.toRadians(first.getLongitude());
